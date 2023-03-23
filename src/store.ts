8d7b3d307c4fbc520860e24c328cd37bdd2fb8fc
naluwan <street.baller06@gmail.com>
2023-03-23 20:05:31 +0800

    feat: add store

---
+++ src/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { homeApi } from 'services/homeServices';
+
+export const store = configureStore({
+  reducer: {
+    [homeApi.reducerPath]: homeApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(homeApi.middleware),
+});
+
+export default store;
